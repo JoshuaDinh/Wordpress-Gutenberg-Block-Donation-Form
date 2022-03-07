a890c88301f1756357bd74554a83ed13d39dce84
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-07 14:27:10 -0800

    changed mode to onBlur

--- src/frontend.js
+++ src/frontend.js
@@ -20,7 +20,7 @@ divsToUpdate.forEach((div) => {
 
 function OurComponent(props) {
   const [step, setStep] = useState(0);
-  const methods = useForm({ mode: "all", resolver: yupResolver(schema) });
+  const methods = useForm({ mode: "onBlur", resolver: yupResolver(schema) });
 
   function displaySteps() {
     switch (step) {
