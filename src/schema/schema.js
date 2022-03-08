010d0f0e3961605f09fd45858220cd8e92368a22
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-07 17:04:06 -0800

    split schemas

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,11 +1,13 @@
 import * as yup from "yup";
 
 // User Information
-export const schema = yup.object().shape({
+export const userSchema = yup.object().shape({
   FirstName: yup.string().max(25).required(),
   LastName: yup.string().max(25).required(),
   Email: yup.string().email().required(),
-  //Disable letters on card input
+});
+
+export const paymentSchema = yup.object().shape({
   CreditCardNumber: yup
     .string()
     .typeError("Looks like you forgot some numbers")
