8413f40ca64216dc806cd87f3a89413ad7188b4d
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-08 19:08:27 -0800

    added html markup to match

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import Form from "./components/Form";
 import "./index.scss";
+import Step1 from "./steps/Step1";
+import Step2 from "./steps/Step2";
+import Result from "./steps/Result";
 
 wp.blocks.registerBlockType("makeupnamespace/make-up-block-name", {
   title: "Boilerplate Block",
@@ -15,5 +19,46 @@ wp.blocks.registerBlockType("makeupnamespace/make-up-block-name", {
 });
 
 function EditComponent(props) {
-  return <div className="makeUpYourBlockTypeName"></div>;
+  return (
+    <form className="form">
+      <div className="form__header">
+        <h2>GiveWP</h2>
+        <img alt="img" src="" />
+      </div>
+      <>
+        <div className="form__group">
+          <label className="form__label">First Name:</label>
+          <div className="form__group__wrapper">
+            <input
+              className="form__input"
+              text="text"
+              placeholder="First Name"
+            />
+            <p className="error__message"></p>
+          </div>
+        </div>
+        <div className="form__group">
+          <label className="form__label">Last Name:</label>
+          <div className="form__group__wrapper">
+            <input
+              className="form__input"
+              text="text"
+              placeholder="Last Name"
+            />
+            <p className="error__message"></p>
+          </div>
+        </div>
+        <div className="form__group">
+          <label className="form__label">Email:</label>
+          <div className="form__group__wrapper">
+            <input className="form__input" text="text" placeholder="Email" />
+            <p className="error__message"></p>
+          </div>
+        </div>
+      </>
+      <button id="form__button" tyle="submit">
+        Next Step
+      </button>
+    </form>
+  );
 }
