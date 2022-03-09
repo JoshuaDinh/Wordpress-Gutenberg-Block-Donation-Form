cf3c03386982b6640a8d8f21a87150e3bbeb5772
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-09 13:49:59 -0800

    updated to check digit length for CardNumber and CVV. removed original test function

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,19 +1,32 @@
 import * as yup from "yup";
 
-// User Information
+// Schema for validation
 export const schema = yup.object().shape({
   FirstName: yup.string().max(25).required(),
   LastName: yup.string().max(25).required(),
   Email: yup.string().email().required(),
-  CreditCardNumber: yup
-    .string()
+  CardNumber: yup
+    .number()
+    .test(
+      "len",
+      "Must be exactly 16 digits",
+      (val) => !val || (val && val.toString().length === 16)
+    )
     .typeError("Looks like you forgot some numbers")
+    .nullable()
     .required(),
-  //Apply date formating
   ExpiryDate: yup
     .string()
     .typeError("Somethings fishy about this.")
     .required(""),
-  Cvv: yup.string().typeError("Ooops.. Try 3 digits").required(),
+  cvv: yup
+    .number()
+    .test(
+      "len",
+      "Must be exactly 3 digits",
+      (val) => !val || (val && val.toString().length === 3)
+    )
+    .typeError("Ooops.. Try 3 digits")
+    .required(),
   anonymous: false,
 });
