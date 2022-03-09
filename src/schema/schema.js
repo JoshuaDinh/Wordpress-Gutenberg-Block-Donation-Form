7488c9ce798fdc959851b348ee2aa0afbe74ba4b
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-09 10:55:17 -0800

    combined schemas

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,13 +1,10 @@
 import * as yup from "yup";
 
 // User Information
-export const userSchema = yup.object().shape({
+export const schema = yup.object().shape({
   FirstName: yup.string().max(25).required(),
   LastName: yup.string().max(25).required(),
   Email: yup.string().email().required(),
-});
-
-export const paymentSchema = yup.object().shape({
   CreditCardNumber: yup
     .string()
     .typeError("Looks like you forgot some numbers")
