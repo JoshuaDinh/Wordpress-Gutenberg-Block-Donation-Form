32dafad0e4d1d909f571ba718757ca366f078246
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-10 15:59:17 -0800

    added step counter with functionality to filter steps on block editor

--- src/index.js
+++ src/index.js
@@ -27,13 +27,22 @@ wp.blocks.registerBlockType("makeupnamespace/make-up-block-name", {
 
 function EditComponent(props) {
   const { attributes, setAttributes } = props;
-  const [isChecked, setChecked] = useState(false);
+  const [step, setStep] = useState(0);
 
   function updateTitle(event) {
     setAttributes({ title: event.target.value });
   }
   function updateCheckedField(event) {
     setAttributes({ checked: event.target.value });
+  }
+
+  function updateSteps() {
+    if (step <= 2) {
+      return setStep(step + 1);
+    } else if (step === 3) {
+      return setStep(0);
+    }
+    console.log(step);
   }
 
   return (
@@ -50,30 +59,81 @@ function EditComponent(props) {
           </PanelBody>
         </Panel>
       </InspectorControls>
+
+      <div className="step__count">
+        <div
+          className={`step ${step === 0 && "step__active"}`}
+          onClick={() => setStep(0)}
+        >
+          {step !== 0 ? "Completed" : "Step 1"}
+        </div>
+        <div
+          className={`step ${step === 1 && "step__active"}`}
+          onClick={() => setStep(1)}
+        >
+          {step > 1 ? "Completed" : "Step 2"}
+        </div>
+        <div
+          className={`step ${step === 2 && "step__active"}`}
+          onClick={() => setStep(2)}
+        >
+          {step > 2 ? "Completed" : "Result"}
+        </div>
+      </div>
       <form className="form">
         <div className="form__header">
           <h2>GiveWP</h2>
           <img alt="img" src="image" />
         </div>
-        <div className="form__group">
-          <div className="form__label">First Name:</div>
-          <div className="form__group__wrapper">
-            <div className="form__input">First Name</div>
-          </div>
-        </div>
-        <div className="form__group">
-          <div className="form__label">Last Name:</div>
-          <div className="form__group__wrapper">
-            <div className="form__input">Last Name</div>
-          </div>
-        </div>
-        <div className="form__group">
-          <div className="form__label">Email:</div>
-          <div className="form__group__wrapper">
-            <div className="form__input">Email</div>
-          </div>
-        </div>
-        <div id="form__button" disabled={true}>
+        {/* Step 1 */}
+        {step === 0 ? (
+          <>
+            <div className="form__group">
+              <div className="form__label">First Name:</div>
+              <div className="form__group__wrapper">
+                <div className="form__input">First Name</div>
+              </div>
+            </div>
+            <div className="form__group">
+              <label className="form__label">Last Name:</label>
+              <div className="form__group__wrapper">
+                <input className="form__input" placeholder="Last name" />
+              </div>
+            </div>
+            <div className="form__group">
+              <div className="form__label">Email:</div>
+              <div className="form__group__wrapper">
+                <div className="form__input">Email</div>
+              </div>
+            </div>{" "}
+          </>
+        ) : (
+          <>
+            {" "}
+            <div className="form__group">
+              <div className="form__label">Card Number::</div>
+              <div className="form__group__wrapper">
+                <div className="form__input">xxxx-xxxx-xxxx-xxxx</div>
+              </div>
+            </div>
+            <div className="form__group">
+              <label className="form__label">Exp. Date:</label>
+              <div className="form__group__wrapper">
+                <div className="form__input">Exp. Date</div>
+              </div>
+            </div>
+            <div className="form__group">
+              <div className="form__label">cvv:</div>
+              <div className="form__group__wrapper">
+                <div className="form__input">cvv:</div>
+              </div>
+            </div>{" "}
+          </>
+        )}
+
+        {/* Step 2 */}
+
+        <div id="form__button" onClick={() => updateSteps()}>
           Next Step
         </div>
       </form>
