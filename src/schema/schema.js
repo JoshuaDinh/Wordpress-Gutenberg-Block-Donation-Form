0973345f0def108fc962d389b235f76e0b007ac8
Joshua Dinh <joshuahungdinh@gmail.com>
2022-03-12 18:58:52 -0800

    added character validation

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -2,8 +2,16 @@ import * as yup from "yup";
 
 // Schema for validation
 export const schema = yup.object().shape({
-  FirstName: yup.string().max(25).required(),
-  LastName: yup.string().max(25).required(),
+  FirstName: yup
+    .string()
+    .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
+    .max(25)
+    .required(),
+  LastName: yup
+    .string()
+    .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field ")
+    .max(25)
+    .required(),
   Email: yup.string().email().required(),
   CardNumber: yup
     .number()
@@ -23,7 +31,7 @@ export const schema = yup.object().shape({
       "Please enter the correct format (Max: 2 digits)",
       (val) => !val || (val && val.toString().length === 2)
     )
-    .typeError("Somethings fishy about this.")
+    .typeError("Please enter the correct format")
     .required(""),
   expireYY: yup
     .number()
@@ -32,7 +40,7 @@ export const schema = yup.object().shape({
       "Please enter the correct format (Max: 2 digits)",
       (val) => !val || (val && val.toString().length === 2)
     )
-    .typeError("Somethings fishy about this.")
+    .typeError("Please enter the correct format")
     .required(""),
   cvv: yup
     .number()
@@ -42,41 +50,7 @@ export const schema = yup.object().shape({
       "Must be exactly 3 digits",
       (val) => !val || (val && val.toString().length === 3)
     )
-    .typeError("Ooops.. Try 3 digits")
+    .typeError("Please enter the correct format")
     .required(),
   anonymous: false,
 });
-// import * as yup from "yup";
-
-// // Schema for validation
-// export const schema = yup.object().shape({
-//   FirstName: yup.string().max(25).required(),
-//   LastName: yup.string().max(25).required(),
-//   Email: yup.string().email().required(),
-//   CardNumber: yup
-//     .number()
-//     // Test number of characters in input
-//     .test(
-//       "len",
-//       "Must be exactly 16 digits",
-//       (val) => !val || (val && val.toString().length === 16)
-//     )
-//     .typeError("Looks like you forgot some numbers")
-//     .nullable()
-//     .required(),
-//   ExpiryDate: yup
-//     .string()
-//     .typeError("Somethings fishy about this.")
-//     .required(""),
-//   cvv: yup
-//     .number()
-//     // Test number of characters in input
-//     .test(
-//       "len",
-//       "Must be exactly 3 digits",
-//       (val) => !val || (val && val.toString().length === 3)
-//     )
-//     .typeError("Ooops.. Try 3 digits")
-//     .required(),
-//   anonymous: false,
-// });
